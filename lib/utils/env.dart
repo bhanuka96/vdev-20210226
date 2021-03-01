f50b37c58d350eeb0fcfe0371345158159a16ff8
Bhanuka Sampath <bhanukas@orelit.com>
2021-03-01 15:03:54 +0530

    env file added

---
+++ lib/utils/env.dart
@@ -0,0 +1,6 @@
+//gitignore or adminAccount from remote config
+import 'package:flutter/material.dart';
+
+class Env{
+  static Map<String, String> adminAccount = <String, String>{'joe@black.lk': 'Jo45*78', 'amal@acme.lk': 'La79*!_io', 'peter@pan.lk': 'Nap42-24'};
+}
